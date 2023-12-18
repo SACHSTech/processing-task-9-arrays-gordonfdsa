028d5525a08697e5e281a97053135f324f24e451
Gordon <gordon.zhu25@ycdsbk>
2023-12-18 00:47:33 -0500

    updated code

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import processing.core.PApplet;
 
 /**
- * Main class to execute sketch
- * @author 
+ * Snowfall game
+ * @author: Gordon Z
  *
  */
 class Main {
@@ -11,6 +11,7 @@ class Main {
     String[] processingArgs = {"MySketch"};
 	  Sketch mySketch = new Sketch();
 	  PApplet.runSketch(processingArgs, mySketch);
+    
   }
   
 }
